03aaac775a6d45f8aa6a4c17138fbd2bc9ce3587
Joao Victor Oliveira <joaovictoroliveira1452@gmail.com>
2023-03-28 10:33:49 -0300

    add: container file

---
+++ plf.h
@@ -0,0 +1,16 @@
+/*
+This is a container to all .h of plf library
+*/
+
+#ifndef D0C34C37_5E9A_4C21_A36F_204FED657959
+#define D0C34C37_5E9A_4C21_A36F_204FED657959
+
+#include "plf_colony.h"
+#include "plf_indiesort.h"
+#include "plf_list.h"
+#include "plf_nanotimer.h"
+#include "plf_queue.h"
+#include "plf_rand.h"
+#include "plf_stack.h"
+
+#endif /* D0C34C37_5E9A_4C21_A36F_204FED657959 */
